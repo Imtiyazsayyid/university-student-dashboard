ec53d7ce7dbea599336f4d6a2e10411af4c6bc6c
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-08-08 11:05:42 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@chakra-ui/react";
 import Image from "next/image";
 import Navbar from "./Navbar";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/student");
   return <main className="min-h-screen"></main>;
 }
