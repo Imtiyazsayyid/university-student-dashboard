a1724131f886b56fc03eae79515eff055d3eb113
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2025-03-07 10:17:20 +0530

    Remove Routes

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -65,8 +65,8 @@ const LinkItems: Array<LinkItemProps> = [
   { name: "Assignments", icon: MdAssignment, route: "/student/assignments", key: "assignments" },
   { name: "Events", icon: BsTicketPerforatedFill, route: "/student/events", key: "events" },
   // {name: "Chats", icon: BsChatSquareText, route: "/student/chats", key: "chats"},
-  { name: "Students", icon: LuUsers, route: "/student/chats", key: "students" },
-  { name: "Conversations", icon: BiSolidConversation, route: "/student/conversations", key: "conversations" },
+  // { name: "Students", icon: LuUsers, route: "/student/chats", key: "students" },
+  // { name: "Conversations", icon: BiSolidConversation, route: "/student/conversations", key: "conversations" },
 ];
 
 const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
