db2daa24db8f38a2ccfb2b6f5c76ac240acda2f6
Fahad Khan <fahad.khan44604@gmail.com>
2025-04-21 20:10:29 +0530

    fix the ui bug

--- app/student/chats/components/EmptyState.tsx
+++ app/student/chats/components/EmptyState.tsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const EmptyState = () => {
   return (
-    <div
-      className="dark:bg-[#151515] px-4 py-10 sm:px-6 lg:px-10 h-full flex justify-center items-center overflow-hidden bg-gray-100"
-    >
+    <div className="dark:bg-[#151515] px-4 py-10 sm:px-6 lg:px-10 h-[933px] flex justify-center items-center overflow-hidden bg-gray-100">
       <div className="flex flex-col text-center items-center">
-        <h3 className="dark:text-white mt-2 text-2xl font-semibold text-gray-900">
+        <h3 className="mt-2 text-2xl font-semibold text-gray-900 dark:text-white">
           Select a chat or start a new conversation
         </h3>
       </div>
